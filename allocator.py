7999fc9ba1fc4179faee44550b7730b71fcaf794
Adem Bayraktar <ademsbayraktar@gmail.com>
2024-10-22 20:18:43 +0100

    added logging

--- allocator.py
+++ allocator.py
@@ -3,7 +3,12 @@ This script allocates secret santa recipients.
 """
 
 from dataclasses import dataclass
+import logging
 from pathlib import Path
+
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 
 @dataclass
 class Pairing:
@@ -15,10 +20,12 @@ class Pairing:
 def import_group(filepath: Path) -> list[Pairing]:
     """imports the names in the group file into a list of pairings"""
     pairings = []
+    LOGGER.info(f'Importing from file: "{filepath}"')
     with open(filepath) as groupfile:
         for line in groupfile.readlines():
             pairings.append(Pairing(line.strip()))
+    LOGGER.info(f'Imported {len(pairings)} names')
+    LOGGER.debug(f'{pairings=}')
     return pairings
 
-print(import_group(Path('group_2024.txt')))
-
+pairings = import_group('group_2024.txt')
